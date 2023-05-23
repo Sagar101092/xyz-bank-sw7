6b01c6f229870766d01995482ea99d16ccfde04b
Sagar101092 <Samgowswami90@gmail.com>
2023-05-23 23:32:03 +0100

    Week16 homework

---
+++ src/main/java/com/way2automation/pages/OpenAccountPage.java
@@ -0,0 +1,17 @@
+package com.way2automation.pages;
+
+import com.way2automation.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created By Kashyap patel
+ */
+public class OpenAccountPage extends Utility {
+    private static final Logger log = LogManager.getLogger(OpenAccountPage.class.getName());
+
+    public OpenAccountPage() {
+        PageFactory.initElements(driver,this);
+    }
+}
---
+++ src/test/java/com/way2automation/steps/BackgroundSteps.java
@@ -0,0 +1,13 @@
+package com.way2automation.steps;
+
+import cucumber.api.java.en.Given;
+
+/**
+ * Created By Kashyap patel
+ */
+public class BackgroundSteps {
+    @Given("^I am on homepage$")
+    public void iAmOnHomepage() {
+
+    }
+}
